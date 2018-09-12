1de35885a24d78d848c44988d1a450280714fbab
Jesse Cardone <jessecar96@gmail.com>
2018-09-11 23:43:44 -0400

    Added gulp workflow for sass

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(cleanCSS())
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('webroot/css'));
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('watch', function () {
+  gulp.watch('scss/*.scss', ['sass']);
+});
