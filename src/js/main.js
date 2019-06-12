c7d64b7c77b02501da674133e86f54a26b0cdb58
Jesse Cardone <jessecar96@gmail.com>
2019-06-12 19:33:34 -0400

    Parse location from window hash

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 WxData.init();
 
-WxData.observations('USNY0483:1:US').then(data => {
+// Default to Atlanta
+let mainLoc = window.location.hash ? window.location.hash : 'USGA0028:1:US';
+
+WxData.observations(mainLoc).then(data => {
   document.getElementById('city').innerText = data.obs_name;
   document.getElementById('current-temp').innerText = data.temp;
   document.getElementById('current-info').innerText = data.wx_phrase;
