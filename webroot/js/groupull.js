a3b063e9d277437459acc97de173aee36cf84d5c
BuildTools <unconfigured@null.spigotmc.org>
2021-08-30 22:46:52 -0400

    fix thing

--- webroot/js/groupull.js
+++ webroot/js/groupull.js
@@ -36,7 +36,6 @@ function GroupDataManager() {
 
 
 			// woeid is the id for the location to pull data for
-			https://api.weather.com/v3/aggcommon/v2fcstdaily3;v2obs;v3-location-point?geocodes=34.44,-83.00;41.40,-38.19&language=en-US&units=e&format=json&apiKey=yourApiKey
 			var url = 'https://api.weather.com/v3/aggcommon/v3-wx-observations-current;v3-location-point?geocodes=41.881832,-87.623177;44.986656,-93.258133;33.427204,-111.939896;46.877186,-96.789803;34.187042,-118.381256;33.660057,-117.998970;36.114647,-115.172813;21.315603,-157.858093;28.538336,-81.379234;43.0,-75.0;&language=en-US&units=e&format=json&apiKey='+ api_key
 
 			pullData(url);
